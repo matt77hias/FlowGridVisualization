2d4fead2bd7d66a5d7dd99ab382b6474d0473496
ALIEN <matthias.moulin@gmail.com>
2016-04-06 15:37:59 +0200

    Added FlowGrid

---
+++ src/flowgrid.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+###################################################################################################################################################################################
+## FlowGridVoxel
+###################################################################################################################################################################################
+class FlowGridVoxel(object):
+    
+    def __init__(self):
+        self.set_access_counts()
+    
+    def set_access_counts(self, access_counts=np.zeros((7))):
+        self.access_counts = access_counts
+   
+###################################################################################################################################################################################
+## FlowGrid
+###################################################################################################################################################################################     
+class FlowGrid(object):
+    
+    def __init__(self, resolution, pMin=np.ones((3)), pMax=np.ones((3))):
+        # AABB data
+        self.pMin = pMin
+        self.pMax = pMax
+        # Grid Voxels
+        self.voxels = np.empty(resolution, dtype=FlowGridVoxel)
+        flatten = self.voxels.flat
+        for index in range(len(flatten)):
+            flatten[index] = FlowGridVoxel()
+        
+    def __getitem__(self, index):
+        return self.voxels[index]
